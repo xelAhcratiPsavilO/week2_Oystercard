a7121469c54a8dd3df7eca3106998d645d00faf2
Chiaki Mizuta <chiakimizuta61@gmail.com>
2018-02-27 12:21:26 +0000

    added topup method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,9 @@
 class Oystercard
+  attr_reader :balance
+  def initialize(balance: 0)
+    @balance = balance
+  end
+  def topup(amount)
+    @balance += amount
+  end   	
 end  
--- spec/oystercard_class_spec.rb
+++ spec/oystercard_class_spec.rb
@@ -1,3 +1,15 @@
 require "oystercard.rb"
 describe Oystercard do
+  context "money" do	
+    it "shows the amount of money that's left on card" do
+      balance = 0
+      oystercard = Oystercard.new
+      expect(oystercard.balance).to eq balance
+    end
+    it "allows user to top up" do	
+      oystercard = Oystercard.new
+      amount = 100
+      expect{ oystercard.topup(amount) }.to change{ oystercard.balance }.by (amount)
+    end  	
+  end 
 end
