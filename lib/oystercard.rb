2e8b290b1cf9506b074309b9a3e4e4b0419d8a7b
Chiaki Mizuta <chiakimizuta61@gmail.com>
2018-02-27 13:09:50 +0000

    added the limit of £90 to the topup method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,13 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :limit
   def initialize(balance: 0)
     @balance = balance
+    @limit = 90
   end
+
   def topup(amount)
     @balance += amount
+    amount = 10
+    fail "You can't add £#{amount} as it exceeds the limit" if @balance + amount >= limit
   end   	
 end  
--- spec/oystercard_class_spec.rb
+++ spec/oystercard_class_spec.rb
@@ -1,15 +1,25 @@
 require "oystercard.rb"
 describe Oystercard do
-  context "money" do	
+  describe "#balace" do	
     it "shows the amount of money that's left on card" do
       balance = 0
       oystercard = Oystercard.new
       expect(oystercard.balance).to eq balance
     end
+  end
+   
+  describe "#topup" do  
     it "allows user to top up" do	
       oystercard = Oystercard.new
-      amount = 100
+      amount = 10
       expect{ oystercard.topup(amount) }.to change{ oystercard.balance }.by (amount)
-    end  	
+    end  
+
+    it "raises an error when the amount exceeds the limit" do
+      oystercard = Oystercard.new	
+      amount = 10
+      message = "You can't add £#{amount} as it exceeds the limit"
+      expect{ oystercard.topup(oystercard.limit + amount) }.to raise_error message	
+    end
   end 
 end
