525cb74b1bd500c391e01350228a3743dd191c71
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-03-02 18:43:41 +0000

    Create tests for edge cases

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,48 +1,63 @@
 require 'journey'
 
 describe Journey do
-  subject(:journey) {described_class.new(:fake_station)}
-  let(:fake_station) {double :Station}
+  #subject(:journey) {described_class.new(:fake_station)}
+  let(:fake_station1) {double :Station}
+  let(:fake_station2) {double :Station}
+
+
+
 
   context 'Given an entry station'do
-    it 'has an entry station recorded' do
-      expect(journey.entry_station).to eq :fake_station
-    end
-    it 'does not have an exit station recorded' do
-      expect(journey.exit_station).to eq nil
-    end
+    subject(:journey) {described_class.new(:fake_station1)}
 
+      it 'has an entry station recorded' do
+        expect(journey.entry_station).to eq :fake_station1
+      end
+      it 'does not have an exit station recorded' do
+        expect(journey.exit_station).to eq nil
+      end
 
+      it 'journey is not complete' do
+        expect(journey.complete?).to be false
+      end
   end
 
   context 'Given an exit station' do
+    subject(:journey) {described_class.new(:fake_station1)}
+
     it 'has an exit station recorded' do
-      journey.finish(:fake_station)
-      expect(journey.exit_station).to eq :fake_station
+      journey.finish(:fake_station1)
+      expect(journey.exit_station).to eq :fake_station1
     end
 
     it 'does not have an entry station recorded' do
-      journey.finish(:fake_station)
+      journey.finish(:fake_station1)
       expect(journey.entry_station).to eq nil
     end
 
+    it 'checks whether the journey is not complete' do
+      expect(journey.complete?).to be false
+    end
+  end
+
+  context 'Given an entry station and an exit station' do
+    subject(:journey) {described_class.new(:fake_station1)}
+    before do
+      journey.finish(:fake_station2)
+    end
+    it 'checks whether the journey is complete' do
+      expect(journey.complete?).to be true
+    end
+  end
+
+  context 'No matter the stations, it finishes the journey' do
+    subject(:journey) {described_class.new(:fake_station1)}
     describe '#finish' do
       it 'finishes the journey by returning the exit station' do
-        expect(journey.finish(:fake_station)).to eq :fake_station
+        expect(journey.finish(:fake_station1)).to eq :fake_station1
       end
     end
-
-    describe '#complete?' do
-
-      it 'is an incomplete journey' do
-        expect(journey.complete?).to be false
-      end
-
-      it 'is a complete journey' do
-        journey.finish(:fake_station)
-        expect(journey.complete?).to be true
-      end
-    end
-
   end
+
 end
